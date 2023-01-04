d8ee918bd962db5714d21fed7af1e0d0bf5b7fcf
p.kamrani <p.kamrani@pikad.com>
2023-01-04 11:43:23 +0330

    add basket api

---
+++ Services/Basket/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Basket.Api.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+            
+        }
+
+        public ShoppingCart(string username)
+        {
+            UserName = username;
+        }
+        public string UserName { get; set; }
+        public List<ShoppingCartItem>Items { get; set; }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var item in Items)
+                {
+                    totalPrice += item.Quantity * item.Price;
+                }
+
+                return totalPrice;
+
+            }
+        }
+    }
+}
---
+++ Services/Basket/Basket.Api/Repository/BasketRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Basket.Api.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Basket.Api.Repository
+{
+    public class BasketRepository:IBasketRepository
+    {
+        private readonly IDistributedCache _cache;
+
+        public BasketRepository(IDistributedCache cache)
+        {
+            _cache = cache; 
+        }
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+           var result= await _cache.GetStringAsync(userName);
+           if (result == null)
+               return null;
+           return JsonSerializer.Deserialize<ShoppingCart>(result);
+
+
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            //var basketUser =await GetBasket(basket.UserName);
+            var basketSerialize = JsonSerializer.Serialize(basket);
+           await _cache.SetStringAsync(basket.UserName, basketSerialize);
+           //await _cache.SetStringAsync(basketUser.UserName, basketSerialize);
+           return basket;
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _cache.RemoveAsync(userName);
+        }
+    }
+}
