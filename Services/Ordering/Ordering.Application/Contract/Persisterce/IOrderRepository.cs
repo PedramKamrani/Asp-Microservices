4d63ba38a0f77052d8d6bd27cf555969d31a4b56
p.kamrani <p.kamrani@pikad.com>
2023-01-07 15:14:52 +0330

    orderng part one

---
+++ Services/Ordering/Ordering.Application/Contract/Persisterce/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ordering.Application.Contract.Persisterce;
+using Ordering.Domain.Entities;
+
+public interface IOrderRepository : IAsyncRepository<Order>
+{
+    Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+}
