16c6e424e8834031f11dcf125e9959c7c822a1a3
p.kamrani <p.kamrani@pikad.com>
2023-01-08 10:39:13 +0330

    26part start

--- Services/Ordering/Ordering.Api/Startup.cs
+++ Services/Ordering/Ordering.Api/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Ordering.Application;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
@@ -25,7 +25,7 @@ namespace Ordering.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            //services.ApplicationServiceRegistration();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
